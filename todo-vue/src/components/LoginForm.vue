04837a7d4e55697611bfc8f2df99c5204cd1aab5
kimseonghoon <ubjhls@naver.com>
2019-11-19 11:45:49 +0900

    Vue | Login Component + vue-session

---
+++ todo-vue/src/components/LoginForm.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+    <form @submit.prevent="login">
+      <label for="username">username : </label>
+      <input v-model="credentials.username" type="text" id="username"><br>
+      <label for="password">password : </label>
+      <input v-model="credentials.password" type="password" id="password">
+      <button type="submit">로그인</button>
+  </form>
+  <!--
+      form 에 이벤트 리스너 달아서,
+      1) 사용자 입력값 출력
+      2) /api-token-auth/로 요청 보내서 토큰값 출력
+   -->
+
+</div>
+</template>
+
+<script>
+export default {
+    name: 'LoginForm',
+    data() {
+        return {
+            credentials: {}
+        }
+    },
+    methods: {
+        login() {
+            this.$emit('login-event', this.credentials)
+            this.credentials = {}
+        }
+    }
+     
+}
+
+
+</script>
+
+<style>
+
+</style>
